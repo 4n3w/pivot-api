6570201e58c90cfa69745ba4ddaedd4ab38dc6ec
4n3w <andrew.d.wood@gmail.com>
2013-01-30 23:35:32 -0500

    Remove DAO layer. Preparation for graph data layer.

--- src/test/java/com/shapelessform/server/dao/DummyDAO.java
+++
@@ -1,9 +0,0 @@
-package com.shapelessform.server.dao;
-
-import com.shapelessform.server.entity.DummyEntity;
-
-/**
- * Plain DAO which provides only {@link com.shapelessform.server.dao.impl.GenericHibernateDAOImpl} methods
- */
-public interface DummyDAO extends GenericDAO<DummyEntity, Long> {
-}
